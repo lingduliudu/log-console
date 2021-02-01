b3059d18ec344ce136fa3b4103baae3120138f2a
lingduliudu <lingduliudu@163.com>
2021-02-01 15:13:48 +0800

    优化

--- src/main/java/tot/log/logconsole/tool/RequestValidTool.java
+++ src/main/java/tot/log/logconsole/tool/RequestValidTool.java
@@ -34,6 +34,9 @@ public class RequestValidTool {
 		if(!StringUtils.isEmpty(s.getEndTime()) && !matchTime(s.getEndTime())) {
 			return true;
 		}
+		if(!StringUtils.isEmpty(s.getGrepContent()) && matchIllegalGrep(s.getGrepContent()) ) {
+			return true;
+		}
 		return false;
 	}
 	
@@ -44,5 +47,17 @@ public class RequestValidTool {
 	public static boolean matchDate(String content) {
 		boolean isMatch = Pattern.matches("^[0-9]{4}-[0-9]{2}-[0-9]{2}$", content);
 		return isMatch;
+	}
+	// 检测grep的合法性
+	public static boolean matchIllegalGrep(String content) {
+		// 匹配一下字符皆违法
+		String[] illegals = {"|","rm "," rm","rm -","/","\\","'","\""};
+		for(String illegal:illegals) {
+			if(content.contains(illegal)) {
+				return true;
+			}
+		}
+		return false;
+		
 	}
 }
